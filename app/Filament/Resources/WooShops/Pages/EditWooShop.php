3205babfd923545b7a861d9e316e162005e7ffc5
Sourov Biswas <sourovcodes@gmail.com>
2026-03-05 17:24:23 +0600

    feat: Add test connection action to EditWooShop page

--- app/Filament/Resources/WooShops/Pages/EditWooShop.php
+++ app/Filament/Resources/WooShops/Pages/EditWooShop.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\WooShops\Pages;
 
 use App\Filament\Resources\WooShops\WooShopResource;
+use App\Models\WooShop;
+use App\Services\WooCommerce\WooOrderSyncService;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditWooShop extends EditRecord
@@ -13,6 +17,26 @@ class EditWooShop extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('test_connection')
+                ->label('Test connection')
+                ->icon('heroicon-o-signal')
+                ->action(function (): void {
+                    /** @var WooShop $record */
+                    $record = $this->record;
+                    $result = app(WooOrderSyncService::class)->testConnection($record);
+
+                    $notification = Notification::make()
+                        ->title($result['success'] ? 'Connection successful' : 'Connection failed')
+                        ->body($result['message']);
+
+                    if ($result['success']) {
+                        $notification->success();
+                    } else {
+                        $notification->danger();
+                    }
+
+                    $notification->send();
+                }),
             DeleteAction::make(),
         ];
     }
